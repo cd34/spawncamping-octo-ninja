6bb3647628d59e13a64bebf738ca4f5549c9f983
Chris Davies <mcd@daviesinc.com>
2013-08-28 15:11:44 +0000

    a bit more robust at handling instance startup and failures

--- backup.py
+++ backup.py
@@ -3,6 +3,8 @@
 import ConfigParser
 import datetime
 import os
+import sys
+import time
 
 import boto.ec2
 from webhelpers.date import distance_of_time_in_words
@@ -32,6 +34,23 @@ Elapsed time: {4}
     if status:
         print 'Sendmail error', status
 
+def wait_for_instance(instance, log):
+    if instance.update() == 'stopped':
+        instance.start()
+    loop = 0
+    while not instance.update() == 'running':
+        time.sleep(10)
+        loop += 1
+        if loop > 10:
+            logger(log, 'Waited and Instance {0} won\'t ' \
+                'start: {1}'.format(instance.id,
+                instance._state.__dict__))
+            return False
+    # even though status is running, machine isn't always accessible
+    # immediately
+    time.sleep(15)
+    return True
+
 def main():
     log = []
     time_start = datetime.datetime.now()
@@ -53,36 +72,49 @@ def main():
     for reservation in reservations:
         for instance in reservation.instances:
             if instance_id in instance.id:
-                if 'running' in instance._state.name:
+                if instance.update() == 'running':
                     public_dns_name =  instance.public_dns_name
                     ip_address = instance.ip_address
                     instance_running = True
                     logger(log, 'Instance {0} is running\n{1} {2}'. \
                         format(instance_id, public_dns_name, ip_address))
                     break
-                if 'stopped' in instance._state.name:
-                    instance.start()
+                elif instance.update() == 'stopped':
                     logger(log, 'Starting Instance {0}'.format(instance_id))
-                    """
-                    delay here waiting for instance to start running
-                    """
-                    public_dns_name =  instance.public_dns_name
-                    ip_address = instance.ip_address
-                    instance_running = True
-                    break
+                    instance_running = wait_for_instance(instance, log)
+                    if instance_running:
+                        logger(log, 'Spawned and Started Instance {0}'.format(instance_id))
+                        public_dns_name =  instance.public_dns_name
+                        ip_address = instance.ip_address
+                        break
+                else:
+                    logger(log, 'Waiting for Instance {0}'.format(instance_id))
+                    instance_running = wait_for_instance(instance, log)
+                    if instance_running:
+                        logger(log, 'Spawned and Started Instance {0}'.format(instance_id))
+                        public_dns_name =  instance.public_dns_name
+                        ip_address = instance.ip_address
+                        break
+                    else:
+                        logger(log, 'Couldn\'t Start Instance {0}'. \
+                            format(instance_id))
+                        break
+                    logger(log, 'Waited and Started Instance {0}'.format(instance_id))
+                break
 
-    rsync_command='{0} -e "ssh -o StrictHostKeyChecking=no" -aplxo --delete ' \
-                    '{1} {2}@{3}:{4}'.format( \
+    if instance_running:
+        rsync_command='{0} -e "ssh -o StrictHostKeyChecking=no" -aplxo ' \
+                '--delete {1} {2}@{3}:{4}'.format( \
                 config.get('backups', 'rsync_path'),
                 config.get('backups', 'backup_source'),
                 config.get('backups', 'remote_username'),
                 public_dns_name, config.get('backups', 'backup_dest'))
 
-    logger(log, 'Running rsync: {0}'.format(rsync_command))
-    os.system(rsync_command)
+        logger(log, 'Running rsync: {0}'.format(rsync_command))
+        os.system(rsync_command)
 
-    logger(log, 'Stopping Instance {0}'.format(instance_id))
-    conn.stop_instances(instance_ids=[instance_id])
+        logger(log, 'Stopping Instance {0}'.format(instance_id))
+        conn.stop_instances(instance_ids=[instance_id])
 
     if email_address:
         email_report(email_address, log, time_start)
