bc3224198b0238146e31a3d629f5fc897660c9f2
Chris Davies <c.gitlab@cd34.com>
2025-04-13 18:27:06 +0000

    modified to use boto3

--- backup.py
+++ backup.py
@@ -7,7 +7,7 @@ import os
 import sys
 import time
 
-import boto.ec2
+import boto3.ec2
 
 
 def logger(log, event):
@@ -41,20 +41,31 @@ Elapsed time: {4}
         print("Sendmail error", status)
 
 
-def wait_for_instance(instance, log):
-    if instance.update() == "stopped":
-        instance.start()
+def get_instance(conn, instance_id):
+    instances = conn.describe_instances()
+    for reservation in instances["Reservations"]:
+        for instance in reservation["Instances"]:
+            if instance_id in instance["InstanceId"]:
+                return instance
+    return "unknown"
+
+
+def wait_for_instance(conn, instance_id, log):
+    instance = get_instance(conn, instance_id)
+    if instance["State"]["Name"] == "stopped":
+        aaa = conn.start_instances(InstanceIds=[instance_id])
     loop = 0
-    while not instance.update() == "running":
+    while not instance["State"]["Name"] == "running":
         time.sleep(10)
         loop += 1
         if loop > 10:
             logger(
                 log,
                 "Waited and Instance {0} won't "
-                "start: {1}".format(instance.id, instance._state.__dict__),
+                "start: {1}".format(instance_id, instance["State"]["Name"]),
             )
             return False
+        instance = get_instance(conn, instance_id)
     # even though status is running, machine isn't always accessible
     # immediately
     time.sleep(60)
@@ -66,14 +77,14 @@ def main():
     time_start = datetime.datetime.now()
     email_address = config.get("backups", "email_address")
 
-    conn = boto.ec2.connect_to_region(
-        config.get("backups", "region"),
+    conn = boto3.client(
+        "ec2",
+        region_name=config.get("backups", "region"),
         aws_access_key_id=config.get("backups", "aws_access_key_id"),
         aws_secret_access_key=config.get("backups", "aws_secret_access_key"),
     )
     logger(log, "Connecting to AWS")
 
-    reservations = conn.get_all_instances()
     instance_id = config.get("backups", "instance_id")
 
     logger(log, "Checking to see if {0} is running".format(instance_id))
@@ -81,46 +92,38 @@ def main():
     public_dns_name = ""
     ip_address = ""
     instance_running = False
-    for reservation in reservations:
-        for instance in reservation.instances:
-            if instance_id in instance.id:
-                if instance.update() == "running":
-                    public_dns_name = instance.public_dns_name
-                    ip_address = instance.ip_address
-                    instance_running = True
-                    logger(
-                        log,
-                        "Instance {0} is running\n{1} {2}".format(
-                            instance_id, public_dns_name, ip_address
-                        ),
-                    )
-                    break
-                elif instance.update() == "stopped":
-                    logger(log, "Starting Instance {0}".format(instance_id))
-                    instance_running = wait_for_instance(instance, log)
-                    if instance_running:
-                        logger(
-                            log, "Spawned and Started Instance {0}".format(instance_id)
-                        )
-                        public_dns_name = instance.public_dns_name
-                        ip_address = instance.ip_address
-                        break
-                else:
-                    logger(log, "Waiting for Instance {0}".format(instance_id))
-                    instance_running = wait_for_instance(instance, log)
-                    if instance_running:
-                        logger(
-                            log, "Spawned and Started Instance {0}".format(instance_id)
-                        )
-                        public_dns_name = instance.public_dns_name
-                        ip_address = instance.ip_address
-                        break
-                    else:
-                        logger(log, "Couldn't Start Instance {0}".format(instance_id))
-                        break
-                    logger(log, "Waited and Started Instance {0}".format(instance_id))
-                break
+    instance = get_instance(conn, instance_id)
+    if instance["State"]["Name"] == "running":
+        public_dns_name = instance["PublicDnsName"]
+        ip_address = instance["PublicIpAddress"]
+        instance_running = True
+        logger(
+            log,
+            "Instance {0} is running\n{1} {2}".format(
+                instance_id, public_dns_name, ip_address
+            ),
+        )
+    elif instance["State"]["Name"] == "stopped":
+        logger(log, "Starting Instance {0}".format(instance_id))
+        instance_running = wait_for_instance(conn, instance_id, log)
+        if instance_running:
+            logger(log, "Spawned and Started Instance {0}".format(instance_id))
+            instance = get_instance(conn, instance_id)
+            public_dns_name = instance["PublicDnsName"]
+            ip_address = instance["PublicIpAddress"]
+    else:
+        logger(log, "Waiting for Instance {0}".format(instance_id))
+        instance_running = wait_for_instance(conn, instance_id, log)
+        if instance_running:
+            logger(log, "Spawned and Started Instance {0}".format(instance_id))
+            public_dns_name = instance["PublicDnsName"]
+            ip_address = instance["PublicIpAddress"]
+        else:
+            logger(log, "Couldn't Start Instance {0}".format(instance_id))
+            return
+        logger(log, "Waited and Started Instance {0}".format(instance_id))
 
+    print("run: ", public_dns_name, ip_address)
     if instance_running:
         rsync_command = (
             '{0} -e "ssh -o StrictHostKeyChecking=no" -aplxo '
@@ -136,8 +139,8 @@ def main():
         logger(log, "Running rsync: {0}".format(rsync_command))
         os.system(rsync_command)
 
-        logger(log, "Stopping Instance {0}".format(instance_id))
-        conn.stop_instances(instance_ids=[instance_id])
+    logger(log, "Stopping Instance {0}".format(instance_id))
+    conn.stop_instances(InstanceIds=[instance_id])
 
     if email_address:
         email_report(email_address, log, time_start)
